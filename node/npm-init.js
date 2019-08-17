59cea29cc5e49189e8b559187156605ac7d2554c
Chris O'Donnell <dev@codfish.io>
2019-08-17 14:01:51 -0400

    feat: abstract node scripts, add npm init module

--- node/npm-init.js
+++ node/npm-init.js
@@ -1,12 +1,14 @@
 const cp = require('child_process');
 
-const priv;
 const username = process.env.GITHUB_USERNAME || 'codfish';
+let priv;
 
 module.exports = {
   name: prompt('name', basename || package.name),
   version: '0.0.0-semantically-released',
-  private: prompt('private', 'false', val => typeof val === 'boolean' ? val : !!val.match('true')),
+  private: prompt('private', 'false', (val) => {
+    return priv = typeof val === 'boolean' ? val : !!val.match('true')
+  }),
   main: prompt('entry point', 'index.js'),
   repository: {
     type: 'git',
@@ -20,8 +22,9 @@ module.exports = {
     if (val[0] !== 'y') return;
 
     console.log('enter github password:');
-    cp.execSync(`curl -u ${username} https://api.github.com/user/repos -d ` +
-      `{ "name": "${basename}", "private": "${priv ? 'true' : 'false'}" }`);
+    cp.execSync("curl -u '" + username + "' https://api.github.com/user/repos -d " +
+      "'{\"name\": \"" + basename + "\", \"private\": " + ((priv) ? 'true' : 'false')  + "}' ");
+    cp.execSync(`git init`);
     cp.execSync(`git remote add origin https://github.com/${username}/${basename}.git`);
   }),
 };
