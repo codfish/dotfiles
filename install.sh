62ccc2333023e5097ecbc56cefb23f6d3479ae75
Chris O'Donnell <dev@codfish.io>
2021-10-07 12:12:44 -0400

    fix: update sh commands to use /bin/bash instead
    This should have broader support

--- install.sh
+++ install.sh
@@ -38,8 +38,8 @@ fi
 # Check for Homebrew
 if ! type 'brew' &> /dev/null; then
   e_header "Installing Homebrew..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
   # make `brew` command available for M1 macs
   if is_m1; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
