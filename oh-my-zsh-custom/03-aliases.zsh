11901546c6065071ae88426bce35642d2182f9a0
Chris O'Donnell <dev@codfish.io>
2022-12-19 21:45:50 -0500

    fix: chromekill alias for latest versions

--- oh-my-zsh-custom/03-aliases.zsh
+++ oh-my-zsh-custom/03-aliases.zsh
@@ -51,7 +51,7 @@ alias hidedeskicons="defaults write com.apple.finder CreateDesktop -bool false &
 
 # Kill all the tabs in Chrome to free up memory
 # [C] explained: http://www.commandlinefu.com/commands/view/402/exclude-grep-from-your-grepped-output-of-ps-alias-included-in-description
-alias chromekill="ps ux | grep '[C]hrome Helper --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
+alias chromekill="ps ux | grep '[C]hrome Helper (Renderer) --type=renderer' | grep -v extension-process | tr -s ' ' | cut -d ' ' -f2 | xargs kill"
 
 # open chromes
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
